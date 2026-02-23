3a8c9b7d9ddfd3ca785bf2470a3f0b6dcce26a69
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2026-02-23 15:51:36 +0800

    更新 test.cpp

--- test.cpp
+++ test.cpp
@@ -1,30 +1,21 @@
 #include <gtest/gtest.h>
-#include "core.hpp"
 
-// 示例测试 1: 测试 ASCII 字符
-TEST(UTF8CharTest, AsciiCharacter) {
-    utf8::UTF8Char ch('A');
-    EXPECT_EQ(ch.bytesize(), 1);
-    EXPECT_EQ(ch.to_cod_point(), 0x41);
-    EXPECT_TRUE(ch.is_alpha());
-    EXPECT_FALSE(ch.is_digit());
+import utf8;
+
+TEST(UTF8CharTest, Ascii) {
+  utf8::UTF8Char c{'A'};
+  EXPECT_EQ(c.bytesize(), 1);
+  EXPECT_EQ(c.to_cod_point(), 'A');
 }
 
-// 示例测试 2: 测试中文字符 (UTF-8 3字节)
-TEST(UTF8CharTest, ChineseCharacter) {
-    // "好" 的 UTF-8 编码是 E5 A5 BD
-    const char* buf = "\xE5\xA5\xBD";
-    utf8::UTF8Char ch(buf, 3);
-    EXPECT_EQ(ch.bytesize(), 3);
-    EXPECT_EQ(ch.to_cod_point(), 0x597D); // "好" 的 Unicode
-    EXPECT_TRUE(ch.is_alpha()); // 你的 is_alpha 包含了 CJK
+TEST(UTF8CharTest, Chinese) {
+  const char* buf = "\xE4\xB8\xAD";
+  utf8::UTF8Char c{buf, 3};
+  EXPECT_EQ(c.bytesize(), 3);
+  EXPECT_EQ(c.to_cod_point(), 0x4E2D);
 }
 
-// 示例测试 3: 测试字符串拼接
-TEST(UTF8StringTest, Concatenation) {
-    utf8::UTF8String s1("Hello");
-    utf8::UTF8String s2(" World");
-    utf8::UTF8String s3 = s1 + s2;
-    EXPECT_EQ(s3.to_string(), "Hello World");
-    EXPECT_EQ(s3.size(), 11); // 字符数
+TEST(UTF8StringTest, Basic) {
+  utf8::UTF8String s{"Hello UTF8"};
+  EXPECT_FALSE(s.empty());
 }
